7d5bff4035e73243316d95db6c618d38db5617c0
Andizzle <andizzle.zhang@gmail.com>
2013-10-29 17:16:42 +1100

    Enable console log according to log level.

--- src/Andizzle/Zapper/Console/ZapperCommand.php
+++ src/Andizzle/Zapper/Console/ZapperCommand.php
@@ -5,10 +5,20 @@ namespace Andizzle\Zapper\Console;
 use Illuminate\Console\Command;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\DB;
 
 class ZapperCommand extends Command {
+
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $verbosity = 0;
+
 
     /**
      * The console command name.
@@ -51,6 +61,36 @@ class ZapperCommand extends Command {
         parent::__construct();
         $this->default_db_type = Config::get('database.default');
         $this->default_db_name = Config::get('database.connections.' . $this->default_db_type . '.database');
+
+    }
+
+
+    public function execute(InputInterface $input, OutputInterface $output) {
+
+        $this->verbosity = $output->getVerbosity();
+        parent::execute($input, $output);
+
+    }
+
+
+    public function info($string) {
+
+        if( $this->verbosity > 1 )
+            parent::info($string);
+
+    }
+
+    public function comment($string) {
+
+        if( $this->verbosity > 2 )
+            parent::comment($string);
+
+    }
+
+    public function error($string) {
+
+        if( $this->verbosity > 3 )
+            parent::error($string);
 
     }
 
