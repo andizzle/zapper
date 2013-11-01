377f8e029b70cc756d13b4d5f3fc651cd261da59
Andizzle <andizzle.zhang@gmail.com>
2013-11-01 17:27:00 +1100

    Add in phpunit overrides.

---
+++ src/Andizzle/Zapper/PHPUnitCommand.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Andizzle\Zapper;
+
+use \PHPUnit_TextUI_Command;
+use Andizzle\Zapper\PHPUnitTestRunner;
+
+class PHPUnitCommand extends PHPUnit_TextUI_Command {
+
+
+    public static function main($exit = TRUE) {
+
+        $command = new PHPUnitCommand;
+        return $command->run($_SERVER['argv'], $exit);
+
+    }
+
+    public function run(array $argv, $exit = TRUE) {
+
+
+        $argv = array();
+        foreach($_SERVER['argv'] as $arg => $val) {
+
+            if( array_key_exists($arg, $this->longOptions) )
+                $argv[$arg] = $val;
+
+        }
+
+        parent::run($argv, $exit);
+
+    }
+
+    protected function createRunner() {
+
+        return new PHPUnitTestRunner($this->arguments['loader']);
+
+    }
+
+}
+
+?>
---
+++ src/Andizzle/Zapper/PHPUnitTestRunner.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Andizzle\Zapper;
+
+use \PHPUnit_TextUI_TestRunner;
+use \PHPUnit_Framework_Test;
+
+
+class PHPUnitTestRunner extends PHPUnit_TextUI_TestRunner {
+
+    public function sortTests(&$suite) {
+
+        if( !$suite instanceof \PHPUnit_Framework_TestSuite )
+            return;
+
+        $tests = $suite->tests();
+        $test_suite = new PHPUnitTestSuite;
+        $test_suite->addTestSuite($suite);
+        $suite = $test_suite;
+
+    }
+
+    public function doRun(\PHPUnit_Framework_Test $suite, array $arguments = array()) {
+
+        $this->sortTests($suite);
+        parent::doRun($suite, $arguments);
+
+    }
+
+}
+
+?>
